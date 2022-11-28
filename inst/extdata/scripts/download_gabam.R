7a65aea50f1912e52b2b543388c96cfc8ac6efec
albhasan <albhasan@gmail.com>
2022-11-28 10:48:47 -0300

    first commit

---
+++ inst/extdata/scripts/download_gabam.R
@@ -0,0 +1,73 @@
+#!/usr/bin/Rscript
+###############################################################################
+# Download GABAM fire data for Brazil
+#------------------------------------------------------------------------------
+# Adapted from https://www.victor-vandermeers.ch/post/r-gabam/
+###############################################################################
+
+library(curl)
+library(tibble)
+library(dplyr)
+
+
+
+#---- Setup ----
+
+out_dir <- "/home/alber/data/gabam/"
+
+years <- 2015:2022
+
+cells <- c("N00W080", "N00W070", "N00W060", "N00W050", "N00W040",
+           "S10W080", "S10W070", "S10W060", "S10W050", "S10W040",
+           "S20W080", "S20W070", "S20W060", "S20W050", "S20W040",
+           "S30W080", "S30W070", "S30W060", "S30W050", "S30W040",
+           "S40W080", "S40W070", "S40W060", "S40W050", "S40W040")
+
+
+
+#---- Utilitary funcitons ----
+
+#' Download GABAM fire data from FTP server 1
+#'
+#' @param years The years to download.
+#' @param cell  The cells to download.
+#' @param dest  The output directory.
+gabam_download <- function(year, cell, out_dir){
+    for(y in year){
+        for(c in cell){
+            url <- paste0("ftp://124.16.184.141/GABAM/burned%20area/",
+                          as.character(y),"/",c,".TIF")
+            file_path <- file.path(out_dir, y, basename(url))
+
+            if (!dir.exists(dirname(file_path)))
+                dir.create(dirname(file_path))
+
+            if (file.exists(file_path) && file.size(file_path) == 0)
+                file.remove(file_path)
+
+            if (!file.exists(file_path)) {
+                h <- curl::new_handle(url = url)
+                curl::multi_add(h, data = file_path)
+            }
+        }
+    }
+    curl::multi_run(timeout = 600)
+}
+
+
+# Download data.
+gabam_download(years, cells, out_dir)
+
+
+# Cleaning.
+out_dir %>%
+    list.files(pattern = "*.(TIF|tif)",
+               recursive = TRUE,
+               full.names = TRUE) %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(file_path = value) %>%
+    dplyr::mutate(file_size = file.size(file_path)) %>%
+    dplyr::filter(file_size == 0) %>%
+    dplyr::pull(file_path) %>%
+    file.remove()
+
