f237803d0560c581508fb2b459b3166df34d38de
albhasan <albhasan@gmail.com>
2023-09-28 20:38:18 -0300

    Validation added.

--- data-raw/00_download_deter.sh
+++ data-raw/00_download_deter.sh
@@ -4,17 +4,19 @@
 ###############################################################################
 
 OUT_DIR="${HOME}/data/terrabrasilis/deter/amazonia_legal"
-
+OUT_FILE="${OUT_DIR}/deter-amz-deter-public.shp"
 
 [ -d "${OUT_DIR}" ] || { echo "ERROR: Directory ${OUT_DIR} does not exist!" >&2; exit 1; }
 
-# Download files.
+if [ -f "${OUT_FILE}" ]; then
+    echo "ERROR: $OUT_FILE already exists." >& 2
+    exit 1
+fi
+
 wget --content-disposition -nc -t 5 http://terrabrasilis.dpi.inpe.br/file-delivery/download/deter-amz/shape -P "${OUT_DIR}" 
 
-# Extract files.
 find "${OUT_DIR}" -type f -iname "*.zip" -exec unzip -o {} -d "${OUT_DIR}" \;
 
-# Remove zip files.
 find "${OUT_DIR}" -type f -iname "*.zip" -exec rm {} \;
 
 exit 0
