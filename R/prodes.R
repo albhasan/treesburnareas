2009c64c8a37acbb8a300c690c68e7878529c40c
albhasan <albhasan@gmail.com>
2026-01-21 13:27:27 -0300

    Improved handling of PRODES legend compatibility.

--- R/prodes.R
+++ R/prodes.R
@@ -22,6 +22,8 @@ compute_prodes_year <- function(adate, start_month = "08", start_day = "01") {
                    FUN.VALUE = integer(1))
         )
     } else if (length(adate) == 1) {
+        if (is.na(adate))
+            return(NA)
         date_year <- lubridate::year(adate)
         prodes_start <- as.Date(paste(date_year, start_month, start_day,
                                       sep = "-"))
@@ -42,51 +44,76 @@ compute_prodes_year <- function(adate, start_month = "08", start_day = "01") {
 #'
 #' @description
 #' This function returns the numeric codes using for the PRODES classes. This
-#' function keeps track of the classes used in PRODES products along the years
-#' (raster and vector).
+#' function keeps track of the classes used in PRODES products (vector and 
+#' raster) along time. 
 #' @return A tibble.
 #' @export
 get_prodes_codes <- function() {
     # TODO: Move this to a configuration file.
     tibble::tribble(
-        ~prodes_code, ~common_name,      ~class_tif_2021,   ~class_shp_2021,
-          7L,         "Deforestation", "d2007 (mascara)", "d2007",
-          8L,         "Deforestation", "d2008",           "d2008",
-          9L,         "Deforestation", "d2009",           "d2009",
-         10L,         "Deforestation", "d2010",           "d2010",
-         11L,         "Deforestation", "d2011",           "d2011",
-         12L,         "Deforestation", "d2012",           "d2012",
-         13L,         "Deforestation", "d2013",           "d2013",
-         14L,         "Deforestation", "d2014",           "d2014",
-         15L,         "Deforestation", "d2015",           "d2015",
-         16L,         "Deforestation", "d2016",           "d2016",
-         17L,         "Deforestation", "d2017",           "d2017",
-         18L,         "Deforestation", "d2018",           "d2018",
-         19L,         "Deforestation", "d2019",           "d2019",
-         20L,         "Deforestation", "d2020",           "d2020",
-         21L,         "Deforestation", "d2021",           "d2021",
-         #
-         32L,         "Cloud",         "Nuvem",           "NUVEM_2021",
-         #
-         50L,         "Residual",      "r2010",           "r2010",
-         51L,         "Residual",      "r2011",           "r2011",
-         52L,         "Residual",      "r2012",           "r2012",
-         53L,         "Residual",      "r2013",           "r2013",
-         54L,         "Residual",      "r2014",           "r2014",
-         55L,         "Residual",      "r2015",           "r2015",
-         56L,         "Residual",      "r2016",           "r2016",
-         57L,         "Residual",      "r2017",           "r2017",
-         58L,         "Residual",      "r2018",           "r2018",
-         59L,         "Residual",      "r2019",           "r2019",
-         60L,         "Residual",      "r2020",           "r2020",
-         61L,         "Residual",      "r2021",           "r2021",
-         #
-         91L,         "Water",         "Hidrografia",     "HIDROGRAFIA",
-         #
-         100L,        "Forest",        "Floresta",        "FOREST_2021",
-         #
-         101L,        "No Forest",     "NaoFloresta",     "NAO_FLORESTA",
-         102L,        "No Forest",     NA,                "NAO_FLORESTA2"
+~prodes_code, ~common_name, ~class_tif_2021, ~class_shp_2021, ~class_tif_2022, ~class_gpkg_2022,
+ 0L,  "Deforestation", NA,                NA,           NA,                      "DESMATAMENTO",
+ 0L,  "Deforestation", NA,                NA,           "d2000",                 "d2000",
+ 2L,  "Deforestation", NA,                NA,           NA,                      "d2002",
+ 4L,  "Deforestation", NA,                NA,           "d2004",                 "d2004",
+ 6L,  "Deforestation", NA,                NA,           "d2006",                 "d2006",
+ 7L,  "Deforestation", "d2007 (mascara)", "d2007",      "d2007",                 "d2007",
+ 8L,  "Deforestation", "d2008",           "d2008",      "d2008",                 "d2008",
+ 9L,  "Deforestation", "d2009",           "d2009",      "d2009",                 "d2009",
+10L,  "Deforestation", "d2010",           "d2010",      "d2010",                 "d2010",
+11L,  "Deforestation", "d2011",           "d2011",      "d2011",                 "d2011",
+12L,  "Deforestation", "d2012",           "d2012",      "d2012",                 "d2012",
+13L,  "Deforestation", "d2013",           "d2013",      "d2013",                 "d2013",
+14L,  "Deforestation", "d2014",           "d2014",      "d2014",                 "d2014",
+15L,  "Deforestation", "d2015",           "d2015",      "d2015",                 "d2015",
+16L,  "Deforestation", "d2016",           "d2016",      "d2016",                 "d2016",
+17L,  "Deforestation", "d2017",           "d2017",      "d2017",                 "d2017",
+18L,  "Deforestation", "d2018",           "d2018",      "d2018",                 "d2018",
+19L,  "Deforestation", "d2019",           "d2019",      "d2019",                 "d2019",
+20L,  "Deforestation", "d2020",           "d2020",      "d2020",                 "d2020",
+21L,  "Deforestation", "d2021",           "d2021",      "d2021",                 "d2021",
+22L,  "Deforestation", NA,                NA,           "d2022",                 "d2022",
+#
+32L,  "Cloud",         "Nuvem",           "NUVEM_2021", NA,                      "NUVEM_2022",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2016",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2017",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2018",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2019",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2020",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2021",
+32L,  NA,              NA,                NA,           NA,                      "NUVEM_2022",
+#
+50L,  "Residual",      NA,                 NA,           NA,                     "residuo",
+50L,  "Residual",      "r2010",           "r2010",       "r2010",                "r2010",
+51L,  "Residual",      "r2011",           "r2011",       "r2011",                "r2011",
+52L,  "Residual",      "r2012",           "r2012",       "r2012",                "r2012",
+53L,  "Residual",      "r2013",           "r2013",       "r2013",                "r2013",
+54L,  "Residual",      "r2014",           "r2014",       "r2014",                "r2014",
+55L,  "Residual",      "r2015",           "r2015",       "r2015",                "r2015",
+56L,  "Residual",      "r2016",           "r2016",       "r2016",                "r2016",
+57L,  "Residual",      "r2017",           "r2017",       "r2017",                "r2017",
+58L,  "Residual",      "r2018",           "r2018",       "r2018",                "r2018",
+59L,  "Residual",      "r2019",           "r2019",       "r2019",                "r2019",
+60L,  "Residual",      "r2020",           "r2020",       "r2020",                "r2020",
+61L,  "Residual",      "r2021",           "r2021",       "r2021",                "r2021",
+62L,  "Residual",      NA,                 NA,           NA,                     "r2022",
+#
+91L,  "Water",         "Hidrografia",     "HIDROGRAFIA", "Hidrografia",          "HIDROGRAFIA",
+91L,  NA,              NA,                NA,            NA,                     "hidrografia",
+#
+100L, "Forest",        "Floresta",        "FOREST_2021", "Floresta",             "FOREST_2022",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2016",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2017",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2018",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2019",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2020",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2021",
+100L, NA,              NA,                NA,              NA,                   "FOREST_2022",
+#
+101L, "No Forest",     "NaoFloresta",     "NAO_FLORESTA",  "Nao Floresta",       "Nao Floresta",
+102L, "No Forest",     NA,                "NAO_FLORESTA2", NA,                   "NAO_FLORESTA2",
+103L, NA,              NA,                NA,              NA,                   "NAO_FLORESTA3",
+#
     ) %>%
     return()
 }
